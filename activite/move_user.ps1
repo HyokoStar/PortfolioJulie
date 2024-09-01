ecc72e51fd000b13752369c27c73cc3e7811c892
HyokoStar <156930986+HyokoStar@users.noreply.github.com>
2024-09-01 13:12:38 +0200

    Add files via upload

---
+++ activite/move_user.ps1
@@ -0,0 +1,36 @@
+# Entrer le chemin de départ et le chemin de fin
+$départ = "A:\depart"
+$fin = "A:\fin"
+
+#Ajout d'un fichier de log dans le répertoire
+$Log = "A:\fin\Log.txt"
+
+# Obtenir tout les noms des dossiers définis dans le chemin de base
+$folders = Get-ChildItem -Path $départ  | Select-Object -First (Get-ChildItem -Path $départ).Count
+
+# Vérification si le fichier de Log existe ou non
+if(Test-Path $Log){
+    Write-Host "Le fichier de Log existe."
+    Remove-Item $Log
+    $Log = New-Item -Path $fin -Name "Log.txt" -ItemType "file"
+} else {
+    $Log = New-Item -Path $fin -Name "Log.txt" -ItemType "file"
+    Write-Host "Le fichier de Log à bien été créer"
+}
+
+# Boucle pour vérifier tout les dossiers un par un
+ForEach ($file in $folders) {
+    $user = Get-Acl -Path $file.FullName | Select-Object -Property  Owner
+
+    # Déplace les dossiers sans propriétaire dans un répertoire spécifiés
+    if($user -like "*nom des users*"){ 
+        Write-Output "Le dossier '$file' à ce propriétaire : $user"
+    } else {
+        Move-Item -Path $file.PSPath -Destination $fin
+        Write-Output "Le dossier à bien été déplacer. '$file' à ce propriétaire : $user"
+
+        # Met les informations dans le fichiers de Log
+        $file | Out-File -FilePath $Log -Append
+        $user | Out-File -FilePath $Log -Append
+    }
+}
